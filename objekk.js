2a3ea2164e6f11c9940812ca85f9480a4bebeb92
Mozzart Destaldio Winar <myname.mozzart@gmail.com>
2026-07-28 12:03:42 +0700

    Add files via upload

---
+++ objekk.js
@@ -0,0 +1,12 @@
+const siswa ={
+    nama:"Mozzart",
+    kelas:10,
+    hobi:"berenang",
+};
+console.log(siswa.nama);
+console.log(siswa.kelas);
+console.log(siswa.hobi);
+
+console.log(siswa[`nama`]);
+console.log(siswa[`kelas`]);
+console.log(siswa[`hobi`]);
