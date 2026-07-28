672a44d4dbd6b6075c71f358e5654479f0773797
Mozzart Destaldio Winar <myname.mozzart@gmail.com>
2026-07-28 08:13:09 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,15 @@
+//Deklarasi Variabel
+let angka = 100; //number
+const sekolah = "SMK Telkom Malang"; //string
+const nama = "Mozzart"
+let kondisi = true;//boolean
+let alamat; //undefined
+let nilai = null;//null
+//output
+console.log(typeof angka); //untuk memunculkan tipe data
+console.log(sekolah);
+console.log(typeof kondisi); //untuk memunculkan tipe data
+console.log(alamat);
+console.log(nilai);
+//template literal
+console.log(`halo namaku ${nama}, aku sekolah di ${sekolah}, rata rata rapot ku ${angka} `);
